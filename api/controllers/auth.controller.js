2cf5af57b7cfa55b8761ce4ab019e618870eebe1
Favour.js <132628823+Arum-Favour@users.noreply.github.com>
2024-08-09 22:24:07 +0100

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -32,6 +32,7 @@ export const register = async (req, res) => {
 
 
 export const login = async (req, res) => {
+  console.log(process.env.JWT_SECRET_KEY);
   const { username, password } = req.body;
   try {
     //CHECK IF USER EXISTS

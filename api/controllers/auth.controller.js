a8d84c9405c9b27bac91f340d5b5661e67175e98
Favour.js <132628823+Arum-Favour@users.noreply.github.com>
2024-08-09 17:56:11 +0100

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -70,6 +70,8 @@ export const login = async (req, res) => {
       })
       .status(200)
       .json(userInfo);
+    console.log("Generated token:", token);
+
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Failed to log in" });

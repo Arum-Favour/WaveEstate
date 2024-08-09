4c878af7aa43d593008c4aaf50550e3814b82e53
Favour.js <132628823+Arum-Favour@users.noreply.github.com>
2024-08-09 16:34:01 +0100

    Update app.js

--- socket/app.js
+++ socket/app.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server({
   cors: {
-    origin: "https://wave-estate-frontend.onrender.com/",
+    origin: "https://wave-estate-frontend.onrender.com",
   },
 });
 
@@ -44,4 +44,4 @@ io.on("connection", (socket) => {
   });
 });
 
-io.listen("4000");
+io.listen("https://wave-estate-backend.onrender.com");

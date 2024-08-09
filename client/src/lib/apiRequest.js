0aadcd5e01934da6f721c96ade847883a896cee0
Favour.js <132628823+Arum-Favour@users.noreply.github.com>
2024-08-09 22:16:56 +0100

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -3,10 +3,6 @@ import axios from "axios";
 const apiRequest = axios.create({
   baseURL: "https://wave-estate-backend.onrender.com",
   withCredentials: true,
-   headers: {
-    "Content-Type": "application/json",
-    "Authorization": `Bearer ${localStorage.getItem("token")}`,
-  },
 });
 
 export default apiRequest;

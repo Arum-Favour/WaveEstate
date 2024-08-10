3c8d0614fef08a7ea773b94e5f375f1b6f7fd675
Favour.js <132628823+Arum-Favour@users.noreply.github.com>
2024-08-10 04:15:36 +0100

    Update app.js

--- api/app.js
+++ api/app.js
@@ -9,14 +9,14 @@ import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 app.use("/api/users", userRoute);
 app.use("/api/chats", chatRoute);

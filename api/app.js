b532535165db924f34779ba3a05693a6e9c1913f
Favour.js <132628823+Arum-Favour@users.noreply.github.com>
2024-08-16 14:15:58 +0100

    Update app.js

--- api/app.js
+++ api/app.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 8800;
 
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true ,  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+app.use(cors({ origin: "*", credentials: true ,  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'], }));
 console.log('CLIENT_URL:', process.env.CLIENT_URL)
 console.log('CLIENT_URL:',  process.env.JWT_SECRET_KEY);

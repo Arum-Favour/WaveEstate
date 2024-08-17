3c3698af6fc02955abce3fde626d6ed6cd276aed
Favour.js <132628823+Arum-Favour@users.noreply.github.com>
2024-08-17 03:13:02 +0100

    Updated verifytoken

--- api/routes/chat.route.js
+++ api/routes/chat.route.js
@@ -9,9 +9,10 @@ import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", verifyToken, getChats);
-router.get("/:id", verifyToken, getChat);
-router.post("/", verifyToken, addChat);
-router.put("/read/:id", verifyToken, readChat);
+//TOOK OFF THE VERIFY TOKEN MIDDLEWARE AS MY HOSTING PLATFORM WASN'T ALLOWING THE PASS OF COOKIES
+router.get("/", getChats);
+router.get("/:id", getChat);
+router.post("/", addChat);
+router.put("/read/:id", readChat);
 
 export default router;

4b2682e0a66e513ea1f3b86688508f3f1c5c8756
Kingson.Arum <nwajiudo@gmail.com>
2024-12-22 03:47:46 +0100

    optimized cookie storage

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800",
+  baseURL: "https://waveestate-1.onrender.com",
   withCredentials: true,
 });
 

3f985fffe2b8ab4b1bc5cc0846e1528e9608746e
Kingson.Arum <nwajiudo@gmail.com>
2024-12-22 03:17:28 +0100

    optimized cookie storage

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("https://socket-server-22db.onrender.com"));
+    setSocket(io("http://localhost:4000"));
   }, []);
 
   useEffect(() => {

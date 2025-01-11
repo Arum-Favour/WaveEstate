1984be153afd3180fe3f8b209e4746302dbce8d5
Favour.js <132628823+Arum-Favour@users.noreply.github.com>
2025-01-11 01:02:33 +0100

    Update notificationStore.js

--- client/src/lib/notificationStore.js
+++ client/src/lib/notificationStore.js
@@ -2,8 +2,10 @@ import { create } from "zustand";
 import apiRequest from "./apiRequest";
 
 export const useNotificationStore = create((set) => ({
+  const token = localStorage.getItem('token');
   number: 0,
   fetch: async () => {
+     'Authorization': `Bearer ${token}`,
     const res = await apiRequest("/api/users/notification");
     set({ number: res.data });
   },
